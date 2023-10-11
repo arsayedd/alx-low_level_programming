9e975f43736d973aba510b63e8541177effe95cd
Ahmed Raafat Sayed <arsayedeg@gmail.com>
2023-10-12 02:32:09 +0300

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,34 +1,21 @@
-/*
- * File: 103-fibonacci.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
-
 /**
- * main - Prints the sum of even-valued Fibonacci sequence
- *        terms not exceeding 4000000.
- *
- * Return: Always 0.
- */
+ *main - prints sum of even fibonacci
+ *suit elements
+ *fibonacci suit numbers
+ *Return: return 0
+*/
 int main(void)
 {
-	unsigned long fib1 = 0, fib2 = 1, fibsum;
-	float tot_sum;
-
-	while (1)
-	{
-		fibsum = fib1 + fib2;
-		if (fibsum > 4000000)
-			break;
-
-		if ((fibsum % 2) == 0)
-			tot_sum += fibsum;
-
-		fib1 = fib2;
-		fib2 = fibsum;
-	}
-	printf("%.0f\n", tot_sum);
-
-	return (0);
+unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+while (n3 <= 4000000)
+{
+n3 = n1 + n2;
+n1 = n2;
+n2 = n3;
+if ((n1 % 2) == 0)
+sum += n1;
+}
+printf("%ld\n", sum);
+return (0);
 }
