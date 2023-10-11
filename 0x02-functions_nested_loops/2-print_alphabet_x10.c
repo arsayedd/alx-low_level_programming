73d79de52a35b188b9df3c5e94d1585dba543a53
Ahmed Raafat Sayed <arsayedeg@gmail.com>
2023-10-12 02:34:50 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *_islower - controls if a character is in lowercase
+ *_isalpha - controls if a character is alphabetical
  *@c: character to be verified
  *Return: return 0 or 1
 */
-int _islower(int c)
+int _isalpha(int c)
 {
-if (c >= 'a' && c <= 'z')
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 return (1);
 return (0);
 }
