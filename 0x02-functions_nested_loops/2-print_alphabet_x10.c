848ce551288259464620d003bc286dbb27f83987
Ahmed Raafat Sayed <arsayedeg@gmail.com>
2023-10-12 02:35:27 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,18 @@
 #include "main.h"
 /**
- *_isalpha - controls if a character is alphabetical
- *@c: character to be verified
- *Return: return 0 or 1
+ *print_alphabet_x10 - prints alphabet letters
+ * ten times
+ *desc: The function uses loop to operate
+ *Return: no return
 */
-int _isalpha(int c)
+void print_alphabet_x10(void)
 {
-if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-return (1);
-return (0);
+int counter = 0;
+int letter;
+while (counter++ <= 9)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar(10);
+}
 }
