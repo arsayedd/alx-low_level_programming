01030d326da35ee8314a546c6ef93bc0f0945042
Ahmed Raafat Sayed <arsayedeg@gmail.com>
2023-10-12 03:08:44 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,30 +1,21 @@
-/*
- * File: 9-print_comb.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
-
+#include <ctype.h>
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ *main - Prints all single digits with , and space followed by new line
  *
- * Return: Always 0.
- */
+ *Return: returns 0
+*/
 int main(void)
 {
-	int num;
-
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
-	}
-
-	putchar('\n');
-
-	return (0);
+int digit;
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+if (digit == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
