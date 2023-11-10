12f587cb92fc77fc53f269f9a5e77dfb51bffa3b
Ahmed Raafat Sayed <arsayedeg@gmail.com>
2023-11-09 18:53:44 -0800

    add readme,0sum_them_all.c files

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,18 @@
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+		int sum = 0;
+			unsigned int i;
+				va_list params;
+
+					va_start(params, n);
+						if (n != 0)
+								{
+											for (i = 0; i < n; i++)
+															sum += va_arg(params, int);
+												}
+							va_end(params);
+
+								return (sum);
+}
