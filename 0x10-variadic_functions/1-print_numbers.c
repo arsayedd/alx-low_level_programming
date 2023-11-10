cb8502ac6a4b071c765a79047263c39840ece4fb
Ahmed Raafat Sayed <arsayedeg@gmail.com>
2023-11-09 19:01:28 -0800

    fix print numbers final

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,13 +1,5 @@
 #include "variadic_functions.h"
 
-/**
- *  * print_numbers -  prints all the parameters passed to it
- *   * @n: number of args passed
- *    * @separator: is the string to be printed between numbers
- *     *
- *      * Return: void
- *       */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 		unsigned int i;
