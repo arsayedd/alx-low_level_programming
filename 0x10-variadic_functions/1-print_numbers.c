e0588e52ea9e74ebd6eefac7c2de50ab4d5793e8
Ahmed Raafat Sayed <arsayedeg@gmail.com>
2023-11-09 19:00:00 -0800

    fix print numbers file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,28 +1,36 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 
+/**
+ *  * print_numbers -  prints all the parameters passed to it
+ *   * @n: number of args passed
+ *    * @separator: is the string to be printed between numbers
+ *     *
+ *      * Return: void
+ *       */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	    if (n == 0)
-		        {
-				        printf("\n");
-					        return;
-						    }
+		unsigned int i;
+			va_list params;
 
-	        unsigned int i;
-		    va_list params;
-
-		        va_start(params, n);
-			    for (i = 0; i < n; i++)
-				        {
-						        printf("%d", va_arg(params, int));
-
-							        if (i != n - 1 && separator != NULL)
-									            printf("%s", separator);
-								    }
-			        printf("\n");
-				    va_end(params);
+				va_start(params, n);
+					if (n != 0)
+							{
+										for (i = 0; i < n; i++)
+													{
+																	if (i != n - 1)
+																					{
+																										if (separator != NULL)
+																																printf("%d%s", va_arg(params, int), separator);
+																														else
+																																				printf("%d", va_arg(params, int));
+																																	}
+																				else
+																								{
+																													printf("%d", va_arg(params, int));
+																																}
+																						}
+											}
+						printf("\n");
+							va_end(params);
 }
-
